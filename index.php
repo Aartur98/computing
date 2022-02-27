a8a50508cc73a7ea7f7b5f27c0b2a7b6b5c319fb
Aartur98 <aarturr98@gmail.com>
2022-02-27 19:14:27 +0100

    Stworzenie strony głównej

--- index.php
+++ index.php
@@ -4,18 +4,60 @@
     <head>
         <meta charset="utf-8">
         <title>Strona główna</title>
+        <link rel="stylesheet" href="styles/index.css">
     </head>
     <body>
         <header>
+            <div id="logo">
+                COMPUTING
+            </div>
             <nav>
-
+                <a href="index.php"><div class="navigation">Strona główna</div></a>
+                <a href="./wydarzenia/wyszukiwarka.php"><div class="navigation">Wydarzenia</div></a>
+                <a href="./zawody/wyniki.php"><div class="navigation">Wyniki</div></a>
+                <a href="./profil/uzytkownicy.php"><div class="navigation">Zawodnicy</div></a>
+                <a href="kontakt.php"><div class="navigation">Kontakt</div></a>
+                <a href="./profil/konto.php"><div class="navigation">Profil</div></a>
             </nav>
         </header>
         <main>
-           abcd 
+            <section id="wydarzenia10">
+                <h3>Najbliższe wydarzenia</h3>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+            </section>
+            <section id="wyniki10">
+                <h3>Ostatnie wyniki</h3>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+                <a href=""><div class="wydarzenie"><img src="sources/images/demo.png" class="miniaturawydarzenie" alt="Miniatura wydarzenia"><p>Nazwa wydarzenia</br>Data wydarzenia</p></div></a>
+            </section>
+            <section id="onas">
+                <h3>O nas</h3>
+                <p>Celem Computing inc. jest zagwarantowanie przyjaznego i funkcjonalnego środowiska udostępniającego narzędzia niezbędne zarządzania wydarzeniem. W tym celu stale pracujemy nad nowymi funkcjonalnościami, gwarantując jednocześnie bezproblemowe działanie obecnych funkcji. Jeśli czujesz, że możesz nam zaufać, serdecznie zapraszamy do dołączenia do grona organizatorów i utworzenie własnego wydarzenia.</p>
+                <a href="profil/rejestracja.php"><button id="przyciskrejestracja">Załóż konto</button></a>
+            </section>
         </main>
         <footer>
-
+            Kontakt:<br>
+            mail:mail@mail.mail<br>
+            phone:543 543 543<br>
+            adres: Paczkowice Mniejsze 7, 36-363 Paczkowice Mniejsze
         </footer>
     </body>
 </html>
