2dd99dbf00dac2e1bd04ecb28be24acb9e3aceee
pieklop <mucha.btz@gmail.com>
2022-02-27 16:41:52 +0100

    Strona kontaktowa

---
+++ kontakt.php
@@ -0,0 +1,44 @@
+<!--strona kontaktowa-->
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Kontakt</title>
+        <link rel="stylesheet" href="styles/kontakt.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <div id="naglowek"> </div>
+            <nav>
+                <div class="lista"><a href="index.php">Strona główna</a></div>
+                <div class="lista"><a href="">Wydarzenia</a></div>
+                <div class="lista"><a href="">Wyniki</a></div>
+                <div class="lista"><a href="">Zawodnicy</a></div>
+                <div class="lista"><a href="kontakt.php">Kontakt</a></div>
+                <div class="lista"><a href="">Profil</a></div>
+            </nav>
+        </header>
+        <main>
+            <h2>Kontakt</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2429.961682719794!2d21.222746516139466!3d52.47982947980662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ec3af70068e7b%3A0xc5507608c7492397!2zV3nFvHN6YSBTemtvxYJhIE1lbGFuxbx1IGltLiBNb2NueWNoIFRydW5rw7N3!5e0!3m2!1spl!2spl!4v1645974380172!5m2!1spl!2spl" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <br><br>
+                Dane kontaktowe:
+                <br>Osiedle Kot 123
+                <br><a href="tel:+48123456789">+48 123-456-789</a>
+                <br><a href="mailto:xyz@gmail.com">xyz@gmail.com</a>
+                <br><br><h3>Zostaw nam wiadomość</h3>
+                <form>
+                Imię i nazwisko:
+                <br><input type="text" name="Imienazwisko">
+                <br>Mail:
+                <br><input type="text" name="mail">
+                <br>Treść wiadomości
+                <br><textarea name="wiadomosc" placeholder="Twoja wiadomosc"></textarea>
+                <br><input type="button" value="Wyślij">
+                </form>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
